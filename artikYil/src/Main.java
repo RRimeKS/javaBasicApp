625e77ff00ffc0d397d9de977b34504ddb07d6d0
Mehmet Emir TAŞKİN <125265094+RRimeKS@users.noreply.github.com>
2023-02-16 16:05:16 +0300

    Artık Yıl Hesaplama...

---
+++ artikYil/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Yıl giriniz...");
+        year = inp.nextInt();
+
+        if (year % 4 == 0) {
+            System.out.println(year + " Yılı bir artık yıldır.");
+        }else{
+            System.out.println(year + " Yılı bir artık yıl değildir.");
+        }
+    }
+}
